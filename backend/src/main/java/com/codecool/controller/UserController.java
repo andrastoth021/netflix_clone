bd922651f47e4e58827055643d0ca286a1604e07
Andras Toth <125831134+andrastoth021@users.noreply.github.com>
2024-02-22 10:18:42 +0100

    implement /register, /signin and a test endpoint /me in UserController

--- backend/src/main/java/com/codecool/controller/UserController.java
+++ backend/src/main/java/com/codecool/controller/UserController.java
@@ -1,36 +1,79 @@
 package com.codecool.controller;
 
+import com.codecool.dto.RegisterDTO;
+import com.codecool.dto.SignInDTO;
+import com.codecool.entity.Role;
+import com.codecool.entity.UserEntity;
+import com.codecool.model.JwtResponse;
+import com.codecool.security.jwt.JwtUtils;
 import com.codecool.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping(path = "/api/users")
+@RequestMapping(path = "/api/user")
 public class UserController {
     private final UserService userService;
+    private final AuthenticationManager authenticationManager;
+    private final JwtUtils jwtUtils;
     private final Logger logger = LoggerFactory.getLogger(UserController.class);
+    private final PasswordEncoder encoder;
 
-    @Autowired
-    public UserController(UserService userService) {
+    public UserController(UserService userService, AuthenticationManager authenticationManager, JwtUtils jwtUtils, PasswordEncoder encoder) {
         this.userService = userService;
+        this.authenticationManager = authenticationManager;
+        this.jwtUtils = jwtUtils;
+        this.encoder = encoder;
     }
 
     @PostMapping("/register")
-    public ResponseEntity<?> registerNewUser() {
-        logger.info("Registration");
-        return new ResponseEntity<>(HttpStatus.OK);
+    public ResponseEntity<?> registerUser(@RequestBody RegisterDTO registerDTO) {
+        if (userService.findByEmail(registerDTO.email()).isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body("User already exists!");
+        }
+
+        UserEntity user = new UserEntity(registerDTO.username(), registerDTO.email(), encoder.encode(registerDTO.password()), Role.ROLE_USER);
+        userService.create(user);
+        logger.info("Registration successful! New user added with username: " + registerDTO.username());
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     @PostMapping("/signin")
-    public ResponseEntity<?> signUserIn() {
-        logger.info("Sign in");
-        return new ResponseEntity<>(HttpStatus.OK);
+    public ResponseEntity<?> authenticateUser(@RequestBody SignInDTO loginRequest) {
+
+        Authentication authentication = authenticationManager
+                .authenticate(new UsernamePasswordAuthenticationToken(loginRequest.username(), loginRequest.password()));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = jwtUtils.generateJwtToken(authentication);
+
+        User userDetails = (User) authentication.getPrincipal();
+        List<String> roles = userDetails.getAuthorities().stream().map(GrantedAuthority::getAuthority)
+                .toList();
+
+        logger.info("User called " + userDetails.getUsername() + " signed in successfully!");
+        return ResponseEntity
+                .ok(new JwtResponse(jwt, userDetails.getUsername(), roles));
     }
 
+    @GetMapping("/me")
+    @PreAuthorize("hasRole('USER')")
+    public String me() {
+        User user = (User) SecurityContextHolder.getContext()
+                .getAuthentication().getPrincipal();
+        return "Hello " + user.getUsername();
+    }
 }
