dcebcab698472d9cba85349122dc67ddbae8ea6b
Andras Toth <125831134+andrastoth021@users.noreply.github.com>
2024-02-22 10:17:45 +0100

    implement UserService; extend UserRepository

--- backend/src/main/java/com/codecool/repository/UserRepository.java
+++ backend/src/main/java/com/codecool/repository/UserRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<UserEntity, Integer> {
-    UserEntity save(UserEntity user);
-
-    Optional<UserEntity> findUserByName(String username);
+    Optional<UserEntity> findUserByUsername(String username);
+    Optional<UserEntity> findUserByEmail(String email);
 }
--- backend/src/main/java/com/codecool/service/UserService.java
+++ backend/src/main/java/com/codecool/service/UserService.java
@@ -1,8 +1,26 @@
 package com.codecool.service;
 
+import com.codecool.entity.UserEntity;
+import com.codecool.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserService {
+    private final UserRepository userRepository;
 
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public void create(UserEntity user) {
+        userRepository.save(user);
+    }
+
+    public Optional<UserEntity> findByEmail(String email) {
+        return userRepository.findUserByEmail(email);
+    }
 }
