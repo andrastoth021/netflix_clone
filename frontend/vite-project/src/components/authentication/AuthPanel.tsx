d12d8120c0fe38d5246718818be666d7f6209f24
Andras Toth <125831134+andrastoth021@users.noreply.github.com>
2024-02-23 19:32:54 +0100

    implement registration (form, toast, fetch..)

--- frontend/vite-project/src/components/authentication/AuthPanel.tsx
+++ frontend/vite-project/src/components/authentication/AuthPanel.tsx
@@ -1,4 +1,7 @@
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+// import { axiosConfig } from "@/config/axios.config.ts";
+import axios from "axios";
 import { Button } from "@/components/ui/button.tsx"
 import {
     Card,
@@ -16,42 +19,141 @@ import {
     TabsList,
     TabsTrigger,
 } from "@/components/ui/tabs.tsx"
+import { toast } from "sonner";
 
 export const AuthPanel = () => {
+    const [formDataSignUp, setFormDataSignUp] = useState({
+        email: '',
+        username: '',
+        password: '',
+    });
+
+    const successfulToast = () => {
+        toast("Successful registration!", {
+            description: `You can now sign in with the following email address: ${formDataSignUp.email}`,
+            action: {
+                label: "Close",
+                onClick: () => console.log("Toast closed"),
+            },
+        })
+    }
+    const notSuccessfulToast = (err: string) => {
+        toast("Something went wrong!", {
+            description: `${err}`,
+            action: {
+                label: "Close",
+                onClick: () => console.log("Toast closed"),
+            },
+        })
+    }
+    const unknownErrorToast = () => {
+        toast("Something went wrong!", {
+            action: {
+                label: "Close",
+                onClick: () => console.log("Toast closed"),
+            },
+        })
+    }
+
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        const { name, value } = event.target;
+        setFormDataSignUp((prevFormData) => ({
+            ...prevFormData,
+            [name]: value,
+        }));
+    };
+
+    const handleSignUp = async (event: React.FormEvent) => {
+        event.preventDefault();
+        try {
+            const data = await axios.post('/api/user/register', {
+                email: formDataSignUp.email,
+                username: formDataSignUp.username,
+                password: formDataSignUp.password
+            });
+            console.log(data);
+            successfulToast();
+        } catch (error) {
+            let errorMessage: string;
+            if (axios.isAxiosError(error)) {
+                // Handle Axios error
+                errorMessage = error.message;
+                if (error.response) {
+                    // The request was made and the server responded with a status code
+                    // that falls out of the range of  2xx
+                    errorMessage = error.response.data;
+                } else if (error.request) {
+                    // The request was made but no response was received
+                    errorMessage = error.request.responseText;
+                } else {
+                    // Something happened in setting up the request that triggered an Error
+                    errorMessage = error.message;
+                }
+                notSuccessfulToast(errorMessage);
+            } else {
+                // Handle other types of errors
+                console.error(error);
+                unknownErrorToast();
+            }
+        }
+    }
 
     return (
-        <form>
+        <>
             <Tabs defaultValue="signup">
                 <TabsList className="grid w-full grid-cols-2">
                     <TabsTrigger value="signup">Sign up</TabsTrigger>
                     <TabsTrigger value="login">Log in</TabsTrigger>
                 </TabsList>
                 <TabsContent value="signup">
-                    <Card>
-                        <CardHeader>
-                            <CardTitle>Create a new account</CardTitle>
-                            <CardDescription>
-                                To get started, please create your account. It's quick and easy.
-                            </CardDescription>
-                        </CardHeader>
-                        <CardContent className="space-y-2">
-                            <div className="space-y-1">
-                                <Label htmlFor="email">Email</Label>
-                                <Input id="email" placeholder="example@example.com" type={"email"} required={true} />
-                            </div>
-                            <div className="space-y-1">
-                                <Label htmlFor="username">Username</Label>
-                                <Input id="username" placeholder="YourUsername" required={true} />
-                            </div>
-                            <div className="space-y-1">
-                                <Label htmlFor="password">Password</Label>
-                                <Input id="password" placeholder="YourPassword" type={"password"} required={true} />
-                            </div>
-                        </CardContent>
-                        <CardFooter>
-                            <Button type={"submit"}>Sign Up!</Button>
-                        </CardFooter>
-                    </Card>
+                    <form onSubmit={handleSignUp}>
+                        <Card>
+                            <CardHeader>
+                                <CardTitle>Create a new account</CardTitle>
+                                <CardDescription>
+                                    To get started, please create your account. It's quick and easy.
+                                </CardDescription>
+                            </CardHeader>
+                            <CardContent className="space-y-2">
+                                <div className="space-y-1">
+                                    <Label htmlFor="email">Email</Label>
+                                    <Input id="email"
+                                       name={"email"}
+                                       type={"email"}
+                                       value={formDataSignUp.email}
+                                       onChange={handleInputChange}
+                                       placeholder="example@example.com"
+                                       required={true}
+                                    />
+                                </div>
+                                <div className="space-y-1">
+                                    <Label htmlFor="username">Username</Label>
+                                    <Input id="username"
+                                       name={"username"}
+                                       type={"username"}
+                                       value={formDataSignUp.username}
+                                       onChange={handleInputChange}
+                                       placeholder="YourUsername"
+                                       required={true}
+                                    />
+                                </div>
+                                <div className="space-y-1">
+                                    <Label htmlFor="password">Password</Label>
+                                    <Input id="password"
+                                        name={"password"}
+                                        type={"password"}
+                                        value={formDataSignUp.password}
+                                        onChange={handleInputChange}
+                                        placeholder="YourPassword"
+                                        required={true}
+                                    />
+                                </div>
+                            </CardContent>
+                            <CardFooter>
+                                <Button type={"submit"}>Sign Up!</Button>
+                            </CardFooter>
+                        </Card>
+                    </form>
                 </TabsContent>
                 <TabsContent value="login">
                     <Card>
@@ -94,6 +196,6 @@ export const AuthPanel = () => {
                 </Link>
                 .
             </p>
-        </form>
+        </>
     )
 }
