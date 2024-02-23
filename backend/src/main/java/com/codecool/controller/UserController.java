b3f19426e6baa664a67aa1b7999387059225d385
Andras Toth <125831134+andrastoth021@users.noreply.github.com>
2024-02-23 20:42:20 +0100

    refactor /register endpoint; put business logic into Service layer from Controller

--- backend/src/main/java/com/codecool/controller/UserController.java
+++ backend/src/main/java/com/codecool/controller/UserController.java
@@ -2,15 +2,12 @@ package com.codecool.controller;
 
 import com.codecool.dto.RegisterDTO;
 import com.codecool.dto.SignInDTO;
-import com.codecool.entity.Role;
 import com.codecool.entity.UserEntity;
 import com.codecool.model.JwtResponse;
 import com.codecool.security.jwt.JwtUtils;
-import com.codecool.service.RoleService;
 import com.codecool.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -20,42 +17,27 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Set;
 
 @RestController
 @RequestMapping(path = "/api/user")
 public class UserController {
     private final UserService userService;
-    private final RoleService roleService;
     private final AuthenticationManager authenticationManager;
     private final JwtUtils jwtUtils;
     private final Logger logger = LoggerFactory.getLogger(UserController.class);
-    private final PasswordEncoder encoder;
 
-    public UserController(UserService userService, RoleService roleService, AuthenticationManager authenticationManager, JwtUtils jwtUtils, PasswordEncoder encoder) {
+    public UserController(UserService userService, AuthenticationManager authenticationManager, JwtUtils jwtUtils) {
         this.userService = userService;
-        this.roleService = roleService;
         this.authenticationManager = authenticationManager;
         this.jwtUtils = jwtUtils;
-        this.encoder = encoder;
     }
 
     @PostMapping("/register")
-    public ResponseEntity<?> registerUser(@RequestBody RegisterDTO registerDTO) {
-        if (userService.findByEmail(registerDTO.email()).isPresent()) {
-            return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body(
-                "The email address you've chosen is already registered!");
-        }
-
-        Role roles = roleService.findByName("ROLE_USER");
-        UserEntity user = new UserEntity(registerDTO.username(), registerDTO.email(), encoder.encode(registerDTO.password()), Set.of(roles));
-        userService.create(user);
-        logger.info("Registration successful! New user added with username: " + registerDTO.username());
-        return ResponseEntity.status(HttpStatus.CREATED).build();
+    public ResponseEntity<?> register(@RequestBody RegisterDTO registerDTO) {
+        return userService.registerUser(registerDTO);
     }
 
     @PostMapping("/signin")
--- backend/src/main/java/com/codecool/service/UserService.java
+++ backend/src/main/java/com/codecool/service/UserService.java
@@ -1,23 +1,50 @@
 package com.codecool.service;
 
+import com.codecool.controller.UserController;
+import com.codecool.dto.RegisterDTO;
+import com.codecool.entity.Role;
 import com.codecool.entity.UserEntity;
 import com.codecool.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
+import java.util.Set;
 
 @Service
 public class UserService {
+    private final RoleService roleService;
     private final UserRepository userRepository;
+    private final PasswordEncoder encoder;
+    private final Logger logger = LoggerFactory.getLogger(UserController.class);
 
     @Autowired
-    public UserService(UserRepository userRepository) {
+    public UserService(RoleService roleService, UserRepository userRepository, PasswordEncoder encoder) {
+        this.roleService = roleService;
         this.userRepository = userRepository;
+        this.encoder = encoder;
     }
 
-    public void create(UserEntity user) {
+    private void create(UserEntity user) {
         userRepository.save(user);
+    }
+
+    public ResponseEntity<?> registerUser(RegisterDTO registerDTO) {
+        if (findByEmail(registerDTO.email()).isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body(
+                "The email address you've chosen is already registered!");
+        }
+
+        Role roles = roleService.findByName("ROLE_USER");
+        UserEntity user = new UserEntity(registerDTO.username(), registerDTO.email(), encoder.encode(registerDTO.password()), Set.of(roles));
+        create(user);
+        logger.info("Registration successful! New user added with username: " + registerDTO.username());
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     public Optional<UserEntity> findByEmail(String email) {
