39ef75028f1c33628c966c7ed9788645a3fd314f
Andras Toth <125831134+andrastoth021@users.noreply.github.com>
2024-02-23 19:32:06 +0100

    implement sonner toast

--- frontend/vite-project/src/main.tsx
+++ frontend/vite-project/src/main.tsx
@@ -2,11 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Application from './Application.tsx'
 import { ThemeProvider } from "@/components/theme-provider"
+import { Toaster } from "@/components/ui/sonner"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <Application />
+      <Toaster />
     </ThemeProvider>
   </React.StrictMode>,
 )
