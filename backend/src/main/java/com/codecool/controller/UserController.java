957175e8e6d0c460664f17983ce438c10699a970
Andras Toth <125831134+andrastoth021@users.noreply.github.com>
2024-02-25 08:37:34 +0100

    remove unused imports

--- backend/src/main/java/com/codecool/controller/UserController.java
+++ backend/src/main/java/com/codecool/controller/UserController.java
@@ -2,24 +2,12 @@ package com.codecool.controller;
 
 import com.codecool.dto.RegisterDTO;
 import com.codecool.dto.SignInDTO;
-import com.codecool.entity.UserEntity;
-import com.codecool.model.JwtResponse;
-import com.codecool.security.jwt.JwtUtils;
 import com.codecool.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping(path = "/api/user")
