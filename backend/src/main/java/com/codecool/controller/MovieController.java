e44fc9a1adfc80a958d05d91344340ee64c9af09
Andras Toth <125831134+andrastoth021@users.noreply.github.com>
2024-03-13 10:46:15 +0100

    implement Movie Controller

---
+++ backend/src/main/java/com/codecool/controller/MovieController.java
@@ -0,0 +1,28 @@
+package com.codecool.controller;
+
+import com.codecool.service.MovieService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping(path = "/api/movie")
+public class MovieController {
+    private final MovieService movieService;
+
+    public MovieController(MovieService movieService) {
+        this.movieService = movieService;
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<?> getAllMovie() {
+        return movieService.getAllMovie();
+    }
+
+    @GetMapping("/details/{uuid}")
+    public ResponseEntity<?> getMovieDetails(@PathVariable UUID uuid) {
+        return movieService.getMovieDetails(uuid);
+    }
+
+}
