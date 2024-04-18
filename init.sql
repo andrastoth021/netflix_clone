ecbbd83f6a249fe802cd8d0a392ca2c805e24e2d
Andras Toth <125831134+andrastoth021@users.noreply.github.com>
2024-04-18 11:01:05 +0200

    add database initialization

---
+++ init.sql
@@ -0,0 +1,6 @@
+INSERT INTO roles(name)
+VALUES('ROLE_USER');
+
+INSERT INTO categories(name)
+VALUES('Action'), ('Adventure'), ('Animation'), ('Comedy'), ('Crime'), ('Drama'),
+      ('Fantasy'), ('Horror'), ('Mystery'), ('Romance'), ('Sci-Fi'), ('Thriller');
